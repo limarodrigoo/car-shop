9ed077195efedad339710b25ebf47278d2f19adf
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-28 13:34:33 -0300

    :bento: create service generic

---
+++ src/services/index.ts
@@ -0,0 +1,32 @@
+import { ZodError } from 'zod';
+import MongoModel from '../models/MongoModel';
+
+export interface ServiceError {
+  error: ZodError,
+}
+
+abstract class Service<T> {
+  constructor(protected model: MongoModel<T>) {}
+
+  public async create(obj: T): Promise<T | null | ServiceError> {
+    return this.model.create(obj);
+  }
+
+  public async read(): Promise<T[]> {
+    return this.model.read();
+  }
+
+  public async readOne(id: string): Promise<T | null | ServiceError> {
+    return this.model.readOne(id);
+  }
+
+  public async update(id: string, obj: T): Promise<T | null | ServiceError> {
+    return this.model.update(id, obj);
+  }
+
+  public async delete(id: string): Promise<T | null | ServiceError> {
+    return this.model.delete(id);
+  }
+}
+
+export default Service;
