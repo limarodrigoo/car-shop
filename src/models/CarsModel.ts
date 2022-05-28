0c39b23074f7ef08d979e0006c6cf3811232fbc3
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-28 13:34:09 -0300

    :bento: create car model

---
+++ src/models/CarsModel.ts
@@ -0,0 +1,21 @@
+import { Schema, model as createModel, Document } from 'mongoose';
+import { Car } from '../interfaces/CarInterface';
+import MongoModel from './MongoModel';
+
+interface CarDocument extends Car, Document {}
+
+const carSchema = new Schema<CarDocument>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+export default class CarModel extends MongoModel<Car> {
+  constructor(model = createModel('Cars', carSchema)) {
+    super(model);
+  }
+}
