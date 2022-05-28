16aa2a5de81e4b878aaf930d826d49ac38192534
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-28 13:33:44 -0300

    :bento: create generic mongo model

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,21 @@
+import { Model as M, Document } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+abstract class MongoModel<T> implements Model<T> {
+  constructor(protected model: M<T & Document>) {}
+
+  create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+
+  read = async (): Promise<T[]> => this.model.find();
+
+  readOne = async (_id: string): Promise<T | null> =>
+    this.model.findOne({ _id });
+
+  update = async (_id: string, obj: T): Promise<T | null> =>
+    this.model.findByIdAndUpdate({ _id }, { ...obj });
+
+  delete = async (_id: string): Promise<T | null> =>
+    this.model.findByIdAndDelete({ _id });
+}
+
+export default MongoModel;
