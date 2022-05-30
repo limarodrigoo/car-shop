23fde7e0a7967d19d3a1d85e6066a1e89e47f6c0
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-30 20:48:23 -0300

    🧪 create read service test

--- src/tests/unit/services/service.test.ts
+++ src/tests/unit/services/service.test.ts
@@ -2,13 +2,17 @@ import { expect } from "chai";
 import CarService from "../../../services/CarService";
 import {
   carDoorsLtTwo,
+  coverageCar,
 } from "../../../../__tests__/utils/CarsMock";
 import Sinon from "sinon";
 import { carSchema } from "../../../interfaces/CarInterface";
+import CarModel from "../../../models/CarsModel";
 
-const carService = new CarService();
 
 describe("CarService", () => {
+  const carModel = new CarModel();
+  
+  const carService = new CarService();
   describe("create with invalid input", () => {
     let parseStub: Sinon.SinonStub;
     before(() => {
@@ -23,5 +27,32 @@ describe("CarService", () => {
       expect(result).to.haveOwnProperty("error");
     });
   });
-  
+  describe("read", () => {
+    let readStub: Sinon.SinonStub;
+
+    before(() => {
+      readStub = Sinon.stub(carModel, "read").resolves([coverageCar]);
+    });
+    after(() => {
+      readStub.restore();
+    });
+    it('returns an array', async () => {
+      const result = await carService.read();
+      expect(result).to.be.equals([coverageCar]);
+    });
+  });
+  describe("readOne", () => {
+    let readStub: Sinon.SinonStub;
+
+    before(() => {
+      readStub = Sinon.stub(carModel, "readOne").resolves(coverageCar);
+    });
+    after(() => {
+      readStub.restore();
+    });
+    it('returns an object', async () => {
+      const result = await carService.readOne('32');
+      expect(result).to.be.equals(coverageCar);
+    });
+  });
 });
