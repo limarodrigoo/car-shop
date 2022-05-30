90e328fef11e09351051342c7ae6ae0a3ff72305
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-30 20:03:06 -0300

    🧪 create first service test

---
+++ src/tests/unit/services/service.test.ts
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import CarService from "../../../services/CarService";
+import {
+  carDoorsLtTwo,
+} from "../../../../__tests__/utils/CarsMock";
+import Sinon from "sinon";
+import { carSchema } from "../../../interfaces/CarInterface";
+import CarModel from '../../../models/CarsModel';
+
+const carService = new CarService();
+const carModel = new CarModel();
+
+describe("CarService", () => {
+  describe("create with invalid input", () => {
+    let parseStub: Sinon.SinonStub;
+    before(() => {
+      parseStub = Sinon.stub(carSchema, "safeParse").resolves(false);
+    });
+    after(() => {
+      parseStub.restore();
+    });
+    it("if invalid input is used", async () => {
+      const result = await carService.create(carDoorsLtTwo);
+
+      expect(result).to.haveOwnProperty("error");
+    });
+  });
+  
+});
